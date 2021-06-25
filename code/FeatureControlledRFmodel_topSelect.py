bcd83de19cfddb594b8357578aab4af9959ca4da
Seo-yeon Kim <66738234+SeoK106@users.noreply.github.com>
2021-06-25 11:42:57 +0900

    Update FeatureControlledRFmodel_topSelect.py

--- code/FeatureControlledRFmodel_topSelect.py
+++ code/FeatureControlledRFmodel_topSelect.py
@@ -1,4 +1,4 @@
-from sklearn import datasets
+from sklearn import datasets, metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
